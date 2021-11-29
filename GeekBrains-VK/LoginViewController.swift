c580cb73cfdc83a1f8b33efcf324791c9d65306a
Maksim Volkov <dev.superstarper@gmail.com>
2021-11-29 21:51:42 +0300

    Create Login Screen and add AppIcon

--- GeekBrains-VK/LoginViewController.swift
+++ GeekBrains-VK/LoginViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var logoSignIn: UIImageView!
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        logoSignIn.image = UIImage(named: "vk_logo")
+        loginTF.placeholder = "E-mail/Номер телефона"
+        passwordTF.placeholder = "Введите пароль"
     }
 
-
+    @IBAction func buttonSignIn(_ sender: UIButton) {
+        if loginTF.text == "admin" && passwordTF.text == "1234" {
+            print("Захады")
+        } else {
+            print("Отказано!")
+        }
+    }
 }
 
